7cdd08eb7618ba1ff9c7e95e9e2ba34492f1ddb3
chicong1425 <chicongcec14@gmail.com>
2026-05-21 18:21:11 +0700

    uncommit databse

--- Database/Update_Receipt_Index.sql
+++
@@ -1,7 +0,0 @@
-USE cloud_pos_db;
-
-ALTER TABLE receipts
-    DROP INDEX uq_receipts_transaction;
-
-ALTER TABLE receipts
-    ADD UNIQUE KEY uq_receipts_tenant_transaction (tenant_id, transaction_id);
